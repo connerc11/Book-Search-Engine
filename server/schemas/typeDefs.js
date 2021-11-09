076358dbf19317c0bbacb41d9475a47f4c62ec00
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-09 17:13:21 -0500

    updating resolvers page

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,21 +4,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type Book {
-    bookId: String,
-    authors: [String],
-    description: String,
-    title: String.
-    image: String,
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
     link: String
 
-},
+}
 
 type User {
-    _id: Id,
-    username: String,
-    email: String,
-    bookCount: Int,
+    _id: Id
+    username: String
+    email: String
+    bookCount: Int
     savedBooks: [Book]
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login:(email, String!, password: String!): Auth
+    addUser:(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookSave!): User
+    removeBook: (bookId: Id!): User
+}
+
+type Auth {
+    token: Id!
+    user: User
 }`;
 
 
